3e8bf6b955da29837b03ee9f4b431bf6816bfa5d
Sannen147 <45742273+Sannen147@users.noreply.github.com>
2020-08-17 23:12:57 -0300

    Update install_files.bat

--- install_files.bat
+++ install_files.bat
@@ -2,6 +2,7 @@
 title Modules Installer
 start cmd /k npm i tmi.js --save
 start cmd /k npm i translation-google --save
+start cmd /k npm i --save-dev nodemon
 echo it's installing modules! don't worry lol 
 echo when they are all done, please close the windows!
 pause>nul
